0190711020f64b9d6a4d2c1cf47d4df3558ed829
Vankani Arjun <65284186+Arjunvankani@users.noreply.github.com>
2023-04-01 23:03:15 +0530

    Create Next Permutation.cpp

---
+++ Day1/Array/Next Permutation.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h> 
+//https://www.codingninjas.com/codestudio/problems/893046?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
+
+vector<int> nextPermutation(vector<int> &p, int n)
+{
+    next_permutation(p.begin(),p.end());
+
+    vector<int>v(p.begin(),p.end());
+
+    return v;
+}
