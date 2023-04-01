f6a7e299026b8d1d917e00b480cbd304b3088bce
Vankani Arjun <65284186+Arjunvankani@users.noreply.github.com>
2023-04-01 23:04:27 +0530

    Create Maximum meetings.cpp

---
+++ Day1/Greddy/Maximum meetings.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+vector<int> maximumMeetings(vector<int> &start, vector<int> &end) {
+    // Write your code here.
+    vector<int> ans;
+    vector<vector<int>> v(start.size());
+
+    for(int i=0;i<v.size();i++){
+      v[i] = {end[i],i+1,start[i]};
+    }
+
+    sort(v.begin(), v.end());
+    int finish = v[0][0];
+
+    ans.push_back(v[0][1]);
+    for(int i=1;i<v.size();i++){
+        if(v[i][2] > finish){
+            finish = v[i][0];
+            ans.push_back(v[i][1]);
+        }
+    }
+    return ans;
+}
