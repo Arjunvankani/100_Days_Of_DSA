20660b2a21d75034eaa7ef0848d2143a8a122b50
Vankani Arjun <65284186+Arjunvankani@users.noreply.github.com>
2023-04-14 23:35:25 +0530

    Add files via upload

---
+++ Day1/String/Implement Atoi Function.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+int atoi(string str) {
+    // Write your code here.
+    int n =str.size();
+    if(n==0) return 0;
+
+    int i=0;
+    while(i<n&& str[i]==' ') i++;
+
+    str = str.substr(i);
+
+    int sign = +1;
+   int  ans =0;
+    if (str[0]=='-') sign =-1;
+   
+    i = (str[0]=='+'|| str[0]=='-') ? 1:0;
+
+      while(i<n){
+        if (isdigit(str[i])) {
+          ans = ans * 10 + str[i] - '0';
+        }
+        i++;
+      }
+    return (sign*ans);
+}
